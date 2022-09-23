5a58b81f19a37c62fc3a86d61bffa54058b2e0a9
Chanfuc <nguoispecial@gmail.com>
2022-09-23 08:25:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,22 +4,93 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>DANG HAI TRANG PHUC</h1>
+      <img width="300" src="https://img.csfd.cz/files/images/user/profile/158/770/158770789_1fd162.jpg"/>
+      <h2>
+        <a href = "https://www.facebook.com/chanfuccute/">https://www.facebook.com/chanfuccute</a></h2>
+      <h3>
+        Hello everyone,
+        <div>
+          Today i feel so good, Welcome to my "raw web site " 🙂
+        </div>
+      </h3>
+      <h4>
+        <img width={200} src="https://scontent.fsgn2-6.fna.fbcdn.net/v/t1.6435-9/193737753_3582409041984566_3021009673068075261_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=xfd2V1ni2EYAX-nHcWx&_nc_ht=scontent.fsgn2-6.fna&oh=00_AT_Ce0EUz67K7E6kQb7BpVqfRqHzvat_jcriZk0Pc8gWkw&oe=63519204"/>
+      </h4>
+       <div>
+           <p>
+               <div>
+                   I'm 22, humorist and handsome guy
+               </div>
+               MY HOBBIES IS 🤟:
+               <div>
+                   <li>
+                       Play Game, Watch Movie, Code
+                   </li>
+               </div>
+               MY APTITUDE IS 💪:
+               <div>
+                   <li>
+                       Chess, Football, Dynamic, Leader
+                   </li>
+               </div>
+               MY CONTACTS IS 📲:
+               <div>
+                   <li>
+                       Youtube: Chanfuc TV
+                   </li>
+                   <li>
+                       Phone Number:+84 19112002
+                   </li>
+                   <li>
+                       Gmail: nguoispecial@gmail.com
+                   </li>
+               </div>
+
+           </p>
+       </div>
+            <table>
+                <tr>
+                    <th>    EDUCATION   </th>
+                    <th>    SKILLS       </th>
+                    <th>    EXPERIENCE    </th>
+                </tr>
+                <tr>
+                    <td>UIT(2019-2023)</td>
+                    <td>Back-end</td>
+                    <td>VNG Company (2023-2024)</td>
+                </tr>
+                <tr>
+                    <td>CISA</td>
+                    <td>Game programmer</td>
+                    <td>Intel (2024-2026)</td>
+                </tr>
+                <tr>
+                    <td>CISSP</td>
+                    <td>Designer & Photoshop</td>
+                    <td>Line (2026-2029)</td>
+                </tr>
+                <tr>
+                    <td>IELTS 7.5</td>
+                    <td>Content Creator</td>
+                    <td>Streamer(Now)</td>
+                </tr>
+            </table>
+        <div>
+            <h5>
+                Anything else you want to ask me ❓
+            </h5>
+            <form>
+                <input type="text"placeholder="          Questions here"/>
+                <button>
+                    Submit a question
+                </button>
+            </form>
+
+        </div>
+
     </div>
-  );
+     );
 }
 
 export default App;
